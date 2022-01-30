0ba078889b06619fd086c2ba2a8a48506e8a53e4
Lord Grosse Jeanine <yoyojeanine@gmail.com>
2022-01-30 13:41:36 +0100

    ongoing socket initializer

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -1,27 +1,35 @@
-import { Server as SocketIoServer } from 'socket.io';
+import { Server as SocketIoServer, Socket } from 'socket.io';
 import { Server } from 'http';
+import { Prisma } from '../services/prisma';
 // import gameRouter from './game';
 import Log from '../services/log';
 import { AuthenticationError } from '../services/errors';
 import { getBearerValidToken } from '../controllers/auth';
 
-const socketRouter = (server: Server) => {
-    const io = new SocketIoServer(server);
+const authMiddleware = async (socket: Socket, next: Function) => {
+    const bearer = socket.handshake.auth.token;
+    const token = await getBearerValidToken(bearer);
+    if (token) {
+        next();
+    } else {
+        next(new AuthenticationError());
+    }
+};
 
-    io.use(async (socket, next) => {
-        const bearer = socket.handshake.auth.token;
-        const token = await getBearerValidToken(bearer);
-        if (token) {
-            next();
-        } else {
-            next(new AuthenticationError());
+const onConnect = async (socket: Socket) => {
+    const { gameId } = socket.handshake.query;
+    /* const game = */await Prisma.game.findUnique({
+        where: {
+            id: gameId as string
         }
     });
+    Log.info('Web socket connected');
+};
 
-    io.on('connection', (/* socket */) => {
-        Log.info('Web socket connected');
-        // const { gameId } = socket.handshake.query;
-    });
+const socketRouter = (server: Server) => {
+    const io = new SocketIoServer(server);
+    io.use(authMiddleware);
+    io.on('connection', onConnect);
 };
 
 export default socketRouter;
